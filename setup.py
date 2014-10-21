71188b783dff414b6cccc2783dde9e575dbbb16b
petschki <pedrotschki@gmail.com>
2014-10-21 21:12:54 +0200

    fix README for github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1'
 setup(name='cs.auth.facebook',
       version=version,
       description="Facebook authentication plugin for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
